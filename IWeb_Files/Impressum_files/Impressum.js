fe0deaf7d099f3f72d37b1f58da23c188c2b7ef5
Mark Stamer <markstamer@gmail.com>
2025-06-07 18:03:04 +0200

    Add site

---
+++ IWeb_Files/Impressum_files/Impressum.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.1 local-build-20100728
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Impressum_files/ImpressumMoz.css')
+Widget.onload();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
