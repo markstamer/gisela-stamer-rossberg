838b2eb18b241a88036a9d9a77a12a5145bd7655
Mark Stamer <markstamer@gmail.com>
2025-06-07 17:59:45 +0200

    gitignore

--- IWeb_Files/Vita_files/Vita.js
+++
@@ -1,9 +0,0 @@
-// Created by iWeb 3.0.1 local-build-20100728
-
-setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
-{return false;}
-function onPageLoad()
-{loadMozillaCSS('Vita_files/VitaMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');Widget.onload();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
-function onPageUnload()
-{Widget.onunload();}
